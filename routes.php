0a5859becd27cc31e64c9ad7e700a976957649ac
DESKTOP-1E42UNI\AFY-temp1 <waiyan@andfun.biz>
2023-02-21 11:12:58 +0630

    update routes.php

--- routes.php
+++ routes.php
@@ -1,21 +1,38 @@
 <?php
 
 require_once 'controllers/UserController.php';
+require_once 'controllers/BookController.php';
 
-// Create userController object
-$userController = new UserController();
+$routes = [
+    '/user' => [
+        'controller' => 'UserController',
+        'method' => 'index'
+    ],
+    '/user/view' => [
+        'controller' => 'UserController',
+        'method' => 'view'
+    ],
+    '/book' => [
+        'controller' => 'BookController',
+        'method' => 'index'
+    ],
+    '/book/view' => [
+        'controller' => 'BookController',
+        'method' => 'view'
+    ]
+];
 
-$action = isset($_GET['action']) ? $_GET['action'] : 'index';
+// Get the incoming url e.g www.example.com/user [/user]
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); //    /user
+$route = $routes[$url]; 
 
-switch ($action) {
-    case 'index':
-        $userController->index();
-        break;
-    case 'view':
-        $userController->view();
-        break;
-    default:
-        echo "Error: Invalid action.";
-        break;
+if($route) {
+    $controller = new $route['controller'](); // new UserController()
+    $method = $route['method']; // index
+    $controller->$method(); // $controller->index()
+}else{
+    header("HTTP/1.0 404 Not Found");
+    echo "404 Not Found";
 }
+
 
